e73487739d874b274f5dca7cf45f74aabd256c38
nguyenviethoang2205 <96973433+nguyenviethoang2205@users.noreply.github.com>
2025-11-16 03:34:24 +0700

    update loi dropdown

--- StoreManagement.Server/Models/Supplier.cs
+++ StoreManagement.Server/Models/Supplier.cs
@@ -15,7 +15,7 @@ public partial class Supplier
 
     public string? Address { get; set; }
 
-    public bool? IsActive { get; set; }
+    public bool IsActive { get; set; } = true; // ← Sửa từ bool? thành bool
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
